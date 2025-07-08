3ac780dbd960a961eeb4cec36a06dab5f9a55645
Jonathan Rugama <jrugama96@gmail.com>
2025-07-07 21:49:27 -0600

    Starting project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pizza = () => {
+    return React.createElement("div", {},[
+        React.createElement("h1", {}, "The Pepperoni Pizza"),
+        React.createElement("p", {}, "Mozzarella Cheese, Pepperoni, Tomato Sauce"),
+    ])
+}
+
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [React.createElement("h1", {}, "Padre Gino's"),
+         React.createElement(Pizza),
+        React.createElement(Pizza),
+        React.createElement(Pizza),
+        React.createElement(Pizza),
+    ]
+        
+    )
+    
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(
+    React.createElement(App)
+);
